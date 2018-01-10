03da87769c432299662ae5945248c0804f12efaf
Tomasz Jonak <tomasz.jonak@forcepoint.com>
2018-01-10 16:29:21 +0100

    Prometheus with proper service discovery, no in app stats yet

--- infrastructure/fetcher/fetcher.py
+++ infrastructure/fetcher/fetcher.py
@@ -5,6 +5,7 @@ import json
 import hashlib
 import random
 import time
+import prometheus_client as pclient
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(name)s|%(threadName)s] %(levelname)s: %(message)s')
 
@@ -16,9 +17,11 @@ redis_host = fetcher.config.get('redis.host', 'localhost')
 redis_port = int(fetcher.config.get('redis.port', 8080))
 red_con = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
 
+fail_counter = pclient.Counter('failed_requests', 'Failed requests', ['method', 'error_type'])
+success_counter = pclient.Counter('success_requests', 'Succeeded requests', ['method'])
+
 try:
     response = red_con.ping()
-    # red_con.rpush('queue:tasks', {'x': 'D'})
     logging.debug('Redis connection established')
 except redis.ConnectionError as e:
     red_con = None
@@ -33,7 +36,12 @@ logging.debug('Hash calculated ({})'.format(secret_hash))
 
 @bottle.route('/', method='GET')
 def health_check():
-    return 'Awsum m8' if red_con else 'No redis connection'
+    if red_con:
+        success_counter.labels('GET').inc()
+        return 'Awsum m8'
+    else:
+        fail_counter.labels('GET', 'redis_failure').inc()
+        return 'No redis connection'
 
 
 @bottle.route('/', method='POST')
@@ -45,22 +53,31 @@ def handle_task():
         decoded_data = json.loads(postdata)
     except Exception as e:
         logging.exception(e)
+        fail_counter.labels('POST', 'payload_decode').inc()
         return bottle.HTTPError(400, 'Not a json formatted post')
 
     if 'secret' not in decoded_data:
+        fail_counter.labels('POST', 'no_secret').inc()
         return bottle.HTTPError(400, 'Required parameter missing')
 
     if decoded_data['secret'] != secret_hash:
+        fail_counter.labels('POST', 'wrong_secret').inc()
         return bottle.HTTPError(403, 'Invalid secret provided')
     time.sleep(random.uniform(0, 5))
-
     if not red_con:
+        fail_counter.labels('POST', 'redis_failure').inc()
         return bottle.HTTPError(503, 'Redis malfunction')
 
     task_data = {k: v for k, v in decoded_data.items() if k not in ('secret',)}
     logging.info('Putting task into queue ({})'.format(task_data))
     red_con.rpush('queue:tasks', task_data)
+    success_counter.labels('POST').inc()
 
+
+# start metrics daemon
+monitoring_port = int(fetcher.config.get('monitoring.port', 8080))
+logging.debug('Starting monitoring service (port: {})'.format(monitoring_port))
+pclient.start_http_server(addr='0.0.0.0', port=monitoring_port)
 
 app_host = fetcher.config.get('frontend.host', 'localhost')
 app_port = int(fetcher.config.get('frontend.port', 80))
